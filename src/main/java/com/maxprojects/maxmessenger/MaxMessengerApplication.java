74977d3ef51dc6995cb2e1eea11241ffa5cd46de
genius <m.kozhuxovskij@g.nsu.ru>
2023-03-23 23:03:17 +0700

    Initial commit

---
+++ src/main/java/com/maxprojects/maxmessenger/MaxMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.maxprojects.maxmessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxMessengerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maxprojects/maxmessenger/MaxMessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxprojects.maxmessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxMessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
