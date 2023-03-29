119aef64b8943477499c0bcbbcfabfdbd3204365
genius <m.kozhuxovskij@g.nsu.ru>
2023-03-29 18:32:57 +0700

    Add userrepository

--- src/main/java/com/maxprojects/maxmessenger/Entities/User.java
+++ src/main/java/com/maxprojects/maxmessenger/Entities/User.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 
 import java.util.Collection;
 import java.util.List;
