c3c7eb0f30cc9e60d40f4f8dcb13a4804823dbc0
genius <m.kozhuxovskij@g.nsu.ru>
2023-03-29 19:58:17 +0700

    add authentcation with jwt

---
+++ src/main/java/com/maxprojects/maxmessenger/config/auth/AuthenticationService.java
@@ -0,0 +1,50 @@
+package com.maxprojects.maxmessenger.config.auth;
+
+import com.maxprojects.maxmessenger.Entities.Role;
+import com.maxprojects.maxmessenger.Entities.User;
+import com.maxprojects.maxmessenger.config.services.JwtService;
+import com.maxprojects.maxmessenger.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request){
+        var user = User.builder()
+                .login(request.getLogin())
+                .mail(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getLogin(),
+                        request.getPassword()
+                )
+        );
+        var user = userRepository.findByLogin(request.getLogin())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
