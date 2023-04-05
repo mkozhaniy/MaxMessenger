4570d4847d03f42e4f63d351389527452904a975
max <45893991+mkozhaniy@users.noreply.github.com>
2023-04-05 20:00:43 +0700

    Delete UserService.java

--- src/main/java/com/maxprojects/maxmessenger/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.maxprojects.maxmessenger.services;
-
-import com.maxprojects.maxmessenger.DTO.UserDTO;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    boolean save(UserDTO userDTO);
-}
